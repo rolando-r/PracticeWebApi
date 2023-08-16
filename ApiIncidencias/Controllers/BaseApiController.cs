b0edab9219a56554c3a8aa2f8e72aa2da3630437
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-16 08:57:29 -0500

    feat: :sparkles: BaseApiController
    Created BaseApiController

---
+++ ApiIncidencias/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiIncidencias.Controllers;
+
+[ApiController]
+[Route("api/incidencias/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    
+}
