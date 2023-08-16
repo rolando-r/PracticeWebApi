a10638f8787d5d63447d7bc1540e82832f5e6df7
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-16 09:20:02 -0500

    feat: :sparkles: IGenericRepository
    Created IGenericRepository

---
+++ Dominio/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace Dominio.Interfaces;
+public interface IGenericRepository<T> where T : BaseEntityA
+{
+    Task<T> GetByIdAsync(string id);
+    Task<IEnumerable<T>> GetAllAsync();
+    IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+    void Add(T entity);
+    void AddRange(IEnumerable<T> entities);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+    void Update(T entity);
+}
