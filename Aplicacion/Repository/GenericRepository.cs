792361c6aea7b5809bd3614961a7907f477d93c2
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-16 15:37:53 -0500

    feat: :construction: BaseRepository
    In construction GenericRepository

--- Aplicacion/Repository/GenericRepository.cs
+++ Aplicacion/Repository/GenericRepository.cs
@@ -1,12 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Aplicacion.Repository
+namespace Aplicacion.Repository;
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntityA
 {
-    public class GenericRepository
+    private readonly PracticeWebApiContext _context;
+
+    public GenericRepository(PracticeWebApiContext context)
     {
-        
+        _context = context;
+    }
+
+    public virtual void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public virtual void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
+    
+    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public Task<T> GetByIdAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Remote(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public virtual void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _context.Set<T>()
+            .Update(entity);
     }
 }
