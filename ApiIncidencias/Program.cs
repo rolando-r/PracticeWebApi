2b5bbd89d9eb8b4df297b3bc4d59b3c5d180a541
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-16 09:04:04 -0500

    feat: :sparkles: CorsPolicy
    Created CorsPolicy

--- ApiIncidencias/Program.cs
+++ ApiIncidencias/Program.cs
@@ -1,3 +1,4 @@
+using ApiIncidencias.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistencia;
 
@@ -9,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.ConfigureCors();
 builder.Services.AddDbContext<PracticeWebApiContext>(options =>
 {
     string connectionString = builder.Configuration.GetConnectionString("ConexMysql");
@@ -24,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
