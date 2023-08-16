d274edb71b2ec5c8768826c075ceb9ca238fc3f8
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-15 23:00:35 -0500

    feat: :sparkles: Context
    Modified context

--- Persistencia/PracticeWebApiContext.cs
+++ Persistencia/PracticeWebApiContext.cs
@@ -1,5 +1,5 @@
+using Dominio;
 using Microsoft.EntityFrameworkCore;
-
 namespace Persistencia
 {
     public class PracticeWebApiContext : DbContext
@@ -7,5 +7,14 @@ namespace Persistencia
         public PracticeWebApiContext(DbContextOptions<PracticeWebApiContext> options) : base(options)
         {
         }
+        public DbSet<Ciudad> ? Ciudades { get; set; }
+        public DbSet<Persona> ? Personas { get; set; }
+        public DbSet<Salon> ? Salones { get; set; }
+        public DbSet<Matricula> ? Matriculas { get; set; }
+        public DbSet<TipoPersona> ? TipoPersonas { get; set; }
+        public DbSet<TrainerSalon> ? TrainerSalones { get; set; }
+        public DbSet<Departamento> ? Departamentos { get; set; }
+        public DbSet<Pais> ? Paises { get; set; }
+        public DbSet<Genero> ? Generos { get; set; }
     }
 }
