aa5db14f1dbe72e3df856118ac82a6ac2bff3152
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-15 22:18:56 -0500

    feat: :sparkles: Context
    Created context

--- ApiIncidencias/Program.cs
+++ ApiIncidencias/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PracticeWebApiContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("ConexMysql");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
--- Persistencia/PracticeWebApiContext.cs
+++ Persistencia/PracticeWebApiContext.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistencia
 {
-    public class PracticeWebApiContext
+    public class PracticeWebApiContext : DbContext
     {
-        
+        public PracticeWebApiContext(DbContextOptions<PracticeWebApiContext> options) : base(options)
+        {
+        }
     }
 }
