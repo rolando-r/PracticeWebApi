dbdfef00b3cdd7c97cf0671f9cc9d721464064e6
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-08-16 09:13:07 -0500

    feat: :sparkles: Migrations
    Created migrations

---
+++ Persistencia/Data/Migrations/20230816140823_InitialCreatingMig.cs
@@ -0,0 +1,345 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistencia.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreatingMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Generos",
+                columns: table => new
+                {
+                    IdGenero = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DescripcionGenero = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Generos", x => x.IdGenero);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Paises",
+                columns: table => new
+                {
+                    IdPais = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NombrePais = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paises", x => x.IdPais);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Salones",
+                columns: table => new
+                {
+                    IdSalon = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NombreSalon = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CapacidadSalon = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salones", x => x.IdSalon);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TipoPersonas",
+                columns: table => new
+                {
+                    IdTipoPersona = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DescripcionTipoPersona = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoPersonas", x => x.IdTipoPersona);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    IdDep = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NombreDep = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdPaisFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PaisIdPais = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.IdDep);
+                    table.ForeignKey(
+                        name: "FK_Departamentos_Paises_PaisIdPais",
+                        column: x => x.PaisIdPais,
+                        principalTable: "Paises",
+                        principalColumn: "IdPais");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Ciudades",
+                columns: table => new
+                {
+                    IdCiudad = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NombreCiudad = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdDepFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DepartamentoIdDep = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudades", x => x.IdCiudad);
+                    table.ForeignKey(
+                        name: "FK_Ciudades_Departamentos_DepartamentoIdDep",
+                        column: x => x.DepartamentoIdDep,
+                        principalTable: "Departamentos",
+                        principalColumn: "IdDep");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    IdPersona = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NombrePersona = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Apellido = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdGeneroFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    GeneroIdGenero = table.Column<int>(type: "int", nullable: true),
+                    IdCiudadFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CiudadIdCiudad = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdTipoPerFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TipoPersonaIdTipoPersona = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.IdPersona);
+                    table.ForeignKey(
+                        name: "FK_Personas_Ciudades_CiudadIdCiudad",
+                        column: x => x.CiudadIdCiudad,
+                        principalTable: "Ciudades",
+                        principalColumn: "IdCiudad");
+                    table.ForeignKey(
+                        name: "FK_Personas_Generos_GeneroIdGenero",
+                        column: x => x.GeneroIdGenero,
+                        principalTable: "Generos",
+                        principalColumn: "IdGenero");
+                    table.ForeignKey(
+                        name: "FK_Personas_TipoPersonas_TipoPersonaIdTipoPersona",
+                        column: x => x.TipoPersonaIdTipoPersona,
+                        principalTable: "TipoPersonas",
+                        principalColumn: "IdTipoPersona");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Direcciones",
+                columns: table => new
+                {
+                    IdDireccion = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TipoVia = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Letra = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SufijoCardinal = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NroViaSecundaria = table.Column<int>(type: "int", nullable: false),
+                    LetraViaSecundaria = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SufijoCards = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdPersonaFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PersonaIdPersona = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Direcciones", x => x.IdDireccion);
+                    table.ForeignKey(
+                        name: "FK_Direcciones_Personas_PersonaIdPersona",
+                        column: x => x.PersonaIdPersona,
+                        principalTable: "Personas",
+                        principalColumn: "IdPersona");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Matriculas",
+                columns: table => new
+                {
+                    IdMatricula = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdPersonaFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PersonaIdPersona = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdSalonFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SalonIdSalon = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matriculas", x => x.IdMatricula);
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Personas_PersonaIdPersona",
+                        column: x => x.PersonaIdPersona,
+                        principalTable: "Personas",
+                        principalColumn: "IdPersona");
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Salones_SalonIdSalon",
+                        column: x => x.SalonIdSalon,
+                        principalTable: "Salones",
+                        principalColumn: "IdSalon");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TrainerSalones",
+                columns: table => new
+                {
+                    IdPerTrainerFk = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PersonaIdPersona = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdSalonFk = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SalonIdSalon = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainerSalones", x => x.IdPerTrainerFk);
+                    table.ForeignKey(
+                        name: "FK_TrainerSalones_Personas_PersonaIdPersona",
+                        column: x => x.PersonaIdPersona,
+                        principalTable: "Personas",
+                        principalColumn: "IdPersona");
+                    table.ForeignKey(
+                        name: "FK_TrainerSalones_Salones_SalonIdSalon",
+                        column: x => x.SalonIdSalon,
+                        principalTable: "Salones",
+                        principalColumn: "IdSalon");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ciudades_DepartamentoIdDep",
+                table: "Ciudades",
+                column: "DepartamentoIdDep");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departamentos_PaisIdPais",
+                table: "Departamentos",
+                column: "PaisIdPais");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Direcciones_PersonaIdPersona",
+                table: "Direcciones",
+                column: "PersonaIdPersona");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_PersonaIdPersona",
+                table: "Matriculas",
+                column: "PersonaIdPersona");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_SalonIdSalon",
+                table: "Matriculas",
+                column: "SalonIdSalon");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_CiudadIdCiudad",
+                table: "Personas",
+                column: "CiudadIdCiudad");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_GeneroIdGenero",
+                table: "Personas",
+                column: "GeneroIdGenero");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_TipoPersonaIdTipoPersona",
+                table: "Personas",
+                column: "TipoPersonaIdTipoPersona");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainerSalones_PersonaIdPersona",
+                table: "TrainerSalones",
+                column: "PersonaIdPersona");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainerSalones_SalonIdSalon",
+                table: "TrainerSalones",
+                column: "SalonIdSalon");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Direcciones");
+
+            migrationBuilder.DropTable(
+                name: "Matriculas");
+
+            migrationBuilder.DropTable(
+                name: "TrainerSalones");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Salones");
+
+            migrationBuilder.DropTable(
+                name: "Ciudades");
+
+            migrationBuilder.DropTable(
+                name: "Generos");
+
+            migrationBuilder.DropTable(
+                name: "TipoPersonas");
+
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+
+            migrationBuilder.DropTable(
+                name: "Paises");
+        }
+    }
+}
